aea29172266172cf4f04011afc450013d816ade8
kocakaysegul <aysegulkocak112@gmail.com>
2023-02-24 09:19:48 +0300

    design completed

---
+++ SimpsonBook/DetailVC.swift
@@ -0,0 +1,35 @@
+//
+//  DetailVC.swift
+//  SimpsonBook
+//
+//  Created by Ayşegül Koçak on 24.02.2023.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var jobLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
